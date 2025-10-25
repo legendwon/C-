9ad6a1e99c722216a49ef43e9945c3492d0e780f
legendwon <won3313310@gmail.com>
2025-10-25 17:04:09 +0900

    5-3완뇨

--- 연습5/practice5-3.c
+++ 연습5/practice5-3.c
@@ -15,3 +15,62 @@ abc bc  aaa  a  g mm  ‘\n’
 
      jksj j    k     lo jj    kk    ’\n’ 등등*/
 
+
+#include <stdio.h>
+
+int main(){
+     //int letter =0;
+     //int avg_letter =0;
+     int all_letter =0;
+     int avg_all_letter =0;
+     int all_word = 0;
+     char ch;
+     int lines =3;
+     
+
+     for(int i=0; i<lines; i++){
+          int letter =0;
+          int avg_letter =0;
+          int word =0;
+          int SY = 0;
+          printf("줄을 입력하시오:");
+          while ((ch= getchar()) != '\n')
+          {
+               if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')) {
+                    letter ++;
+                    SY =1;
+
+               }
+               else if((ch == ' ') || ch =='\t'){
+                    if(SY ==1)
+                    {
+                         word ++;
+                         SY = 0;
+                    }
+                    else if(SY == 0){
+                         
+                    }
+
+               }
+               else if (ch == '\r'){
+                    letter = 0;
+                    word =0;
+                    SY =0;
+               }
+
+          }
+          if (SY ==1){
+               word ++;
+          }
+
+          all_letter += letter;
+          all_word += word;
+          printf("단어의 갯수:%d\n",word);
+          //word가 0일 수 있다는 것을 체크하고 넘어갈 것
+          printf("평균 단어 길이:%.2lf\n",(double)letter / word);
+     }
+
+     printf("전체 단어 갯수: %d\n", all_word);
+     printf("전체 평균 단어 길이%.2lf\n", (double)all_letter/ all_word);
+
+}
